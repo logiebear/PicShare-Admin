94a15170db11566af8a94ebd196ef009c42f1430
Nick Funk <nfunk@usc.edu>
2016-03-19 21:06:32 -0700

    Implemented initial query for photos by user

---
+++ picshare-admintool/test.js
@@ -0,0 +1,6 @@
+function test() {
+  //console.log("test");
+  return "test";
+}
+
+module.exports = test
---
+++ routes/search.js
@@ -0,0 +1,67 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/byuser', function(req, res, next) {
+    var User = req.parse.Object.extend("User");
+    var userQuery = new req.parse.Query(User);
+    userQuery.startsWith("username", req.query.user);
+    var Photo = req.parse.Object.extend("Photo");
+    var photoQuery = new req.parse.Query(Photo);
+    photoQuery.matchesKeyInQuery("owner.objectID", "objectID", userQuery);
+    photoQuery.find({
+        success: function(results) {
+            res.send(results);
+        },
+        error: function(error) {
+            console.log(error);
+            res.send("Some kind of error occurred: " + error);
+        }
+    });
+});
+
+router.get('/allusers', function(req, res, next) {
+    var User = req.parse.Object.extend("User");
+    var query = new req.parse.Query(User);
+    query.find({
+        success: function(results) {
+            console.log(results);
+            res.send(results);
+        },
+        error: function(error) {
+            console.log(error);
+            res.send("Some kind of error occurred: " + error);
+        }
+    });
+});
+
+router.get('/allphotos', function(req, res, next) {
+    var Photo = req.parse.Object.extend("Photo");
+    var query = new req.parse.Query(Photo);
+    query.find({
+        success: function(results) {
+            console.log(results);
+            res.send(results);
+        },
+        error: function(error) {
+            console.log(error);
+            res.send("Some kind of error occurred: " + error);
+        }
+    });
+});
+
+router.get('/allevents', function(req, res, next) {
+    var Event = req.parse.Object.extend("Event");
+    var query = new req.parse.Query(Event);
+    query.find({
+        success: function(results) {
+            console.log(results);
+            res.send(results);
+        },
+        error: function(error) {
+            console.log(error);
+            res.send("Some kind of error occurred: " + error);
+        }
+    });
+});
+
+module.exports = router;
