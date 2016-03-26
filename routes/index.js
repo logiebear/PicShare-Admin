4c86c2d2d2e67dd045009ef2648c9640c5064d17
Weili <weilizha@usc.edu>
2016-03-26 16:52:26 -0700

    frontend base

---
+++ routes/index.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  var data="Express";
+  req.parse.Cloud.run('hello', {}).then(function(result) {
+  // ratings should be 4.5
+  //res.render('index', { title: result['result'] });
+   data = result;
+   //console.log(data);
+   res.render('index', { title: data});
+});
+});
+
+/* GET Hello World page. */
+router.get('/helloworld', function(req, res, next) {
+    res.render('helloworld', { title: 'Hello, World!' });
+});
+
+router.get('/userlist', function(req, res) {
+    var db = req.db;
+    var collection = db.get('_User');
+    console.log(collection);
+    collection.find({},{},function(e,docs){
+    //  console.log(docs);
+        res.render('userlist', {
+            "userlist" : docs
+        });
+    });
+});
+
+module.exports = router;
