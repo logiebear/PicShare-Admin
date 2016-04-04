ea3281ee4c3d67c2d107b46aca9bdba071acc81c
Weili <weilizha@usc.edu>
2016-04-03 17:37:34 -0700

    search for user finished

--- routes/user.js
+++ routes/user.js
@@ -6,37 +6,33 @@ var router = express.Router();
 router.get('/', function(req, res) {
 	var data = {
 		username: "Harry Potter",
-		users: [
-			{userID: "user1"},
-			{userID: "user2"},
-			{userID: "user3"}
-		],
-		photos: [
-			{photoID: "IMG_0518", src:"images/Photos/IMG_0518.jpg", photoUsr: "user1", 
-			eventID: "event2", uploadDate: "03/01/2016", onclick: "select('IMG_0518')"},
-			{photoID: "IMG_0674", src:"images/Photos/IMG_0674.jpg", photoUsr: "user1", 
-			eventID: "event2", uploadDate: "03/01/2016", onclick: "select('IMG_0674')"},
-			{photoID: "IMG_0808", src:"images/Photos/IMG_0808.jpg", photoUsr: "user1", 
-			eventID: "event2", uploadDate: "03/01/2016", onclick: "select('IMG_0808')"},
-			{photoID: "IMG_1082", src:"images/Photos/IMG_1082.jpg", photoUsr: "user1", 
-			eventID: "event2", uploadDate: "03/01/2016", onclick: "select('IMG_1082')"},
-			{photoID: "IMG_1175", src:"images/Photos/IMG_1175.jpg", photoUsr: "user1", 
-			eventID: "event2", uploadDate: "03/01/2016", onclick: "select('IMG_1175')"}
-		]
+		users: [],
+		photos: []
 	};
-
     res.render('user', data);
 });
 
 router.get('/getAllUsers', function(req, res) {
 	var userSearch = req.picshare.userSearch;
-	console.log(userSearch);
-    data = userSearch.getAllUsers(function(result) {
-    	console.log(result);
-    	console.log("test result");
+    userSearch.getAllUsers(function(result) {
     	res.send(result);
     });
 });
 
+router.get('/searchByUser', function(req, res) {
+	var userSearch = req.picshare.userSearch;
+    userSearch.searchByUser(req.param("search"), function(result) {
+    	res.send(result);
+    });
+});
+
+router.get('/fetchUserPhotos', function(req, res) {
+	console.log("userjs");
+	var userSearch = req.picshare.userSearch;
+    userSearch.fetchUserPhotos(req.param("user"), function(result) {
+    	console.log(result);
+    	res.send(result);
+    });
+});
 
 module.exports = router;
